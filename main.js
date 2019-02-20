5f10589d1e38836d27924a8564ca93b21d3a1c41
Duda Duarte <dudammduarte@yahoo.com.br>
2019-02-20 17:14:01 -0300

    vou tentar mudanças drásticas, melhor salvar essa versão

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.getElementById('buttonId').addEventListener('click', getText);
+
 function getText() {
   let userText = document.getElementById('insert-text').value;
   document.getElementById('insert-text').value = '';
@@ -18,7 +20,7 @@ function postText(text) {
 function createProfileInfo() {
   let sectionUser = document.createElement('section');
   sectionUser.className = 'display-flex';
-  let profilePic = document.createElement('spam');
+  let profilePic = document.createElement('div');
   profilePic.className = 'profile-pic';
   let userDiv = document.createElement('div');
   userDiv.className = 'div-user';
@@ -26,8 +28,8 @@ function createProfileInfo() {
   let listDisplayFlex = document.getElementsByClassName('display-flex');
   userDiv.appendChild(userName);
   document.getElementById('feedId').appendChild(sectionUser);
-  document.getElementsByClassName('display-flex')[listDisplayFlex.length-1].appendChild(profilePic);
-  document.getElementsByClassName('display-flex')[listDisplayFlex.length-1].appendChild(userDiv);
+  document.getElementsByClassName('display-flex')[listDisplayFlex.length - 1].appendChild(profilePic);
+  document.getElementsByClassName('display-flex')[listDisplayFlex.length - 1].appendChild(userDiv);
 }
 
 function enableButton() {
@@ -43,28 +45,36 @@ function disableButton() {
 }
 
 function charCountClean() {
-  document.getElementById('remainingCharacters').innerHTML = '';
-}
-
-function changeColorCount() {
-  if (0 < value <= 10) {
-    document.getElementById('remainingCharacters').classList.remove('yellowCounter')
-    document.getElementById('remainingCharacters').classList.add('redCounter');
-  }
-  else if (10 < value <= 20) {
-    document.getElementById('remainingCharacters').classList.add('yellowCounter');
-  }
+  document.getElementById('remainingChar').innerHTML = '';
 }
 
 function charCount() {
+  enableButton();
   let max = 140;
   let current = document.getElementById('insert-text').value.length;
   let value = max - current;
+  changeColorCount(value);
+}
+
+function changeColorCount(value) {
+  let counter = document.getElementById('remainingChar');
   if (value >= 0) {
-    document.getElementById('remainingCharacters').innerHTML = value;
+    counter.classList.remove('redCounter');
+    if (value <= 20 && value > 10) {
+      counter.classList.remove('orangeCounter')
+      counter.classList.add('yellowCounter');
+    } else if (value <= 10 && value >= 0) {
+      counter.classList.remove('yellowCounter')
+      counter.classList.add('orangeCounter');
+    } else {
+      counter.classList.remove('yellowCounter');
+    }
+    counter.innerHTML = value;
   } else {
     document.getElementById('buttonId').disabled = true;
-    document.getElementById('remainingCharacters').innerHTML = value;
+    counter.classList.remove('orangeCounter');
+    counter.classList.add('redCounter');
+    counter.innerHTML = value;
   }
 }
 
