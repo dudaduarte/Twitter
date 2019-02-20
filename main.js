784720f1afcaa194105953db4b8b0c9be32150bc
Duda Duarte <dudammduarte@yahoo.com.br>
2019-02-20 00:31:43 -0300

    mudanças de css já que nao consegui avançar em mais nada

--- main.js
+++ main.js
@@ -1,53 +1,54 @@
 function getText() {
-    let userText = document.getElementById('insert-text').value;
-    document.getElementById('insert-text').value = '';
-    postText(userText);
-    disableButton();
-    charCountClean()
+  let userText = document.getElementById('insert-text').value;
+  document.getElementById('insert-text').value = '';
+  document.getElementsByClassName('feed')[0].classList.remove('hide');
+  postText(userText);
+  disableButton();
+  charCountClean()
 }
 
 function postText(text) {
-    let paragraph = document.createElement("p");
-    let tweet = document.createTextNode(text);
-    paragraph.appendChild(tweet);
-    document.getElementById("feed").appendChild(paragraph);
+  let paragraph = document.createElement("p");
+  let tweet = document.createTextNode('User:\n' + text);
+  paragraph.appendChild(tweet);
+  document.getElementById("feedId").appendChild(paragraph);
 }
 
 function enableButton() {
-    if (document.getElementById('insert-text').value == '') {
-        document.getElementById('buttonId').disabled = true;
-    } else {
-        document.getElementById('buttonId').disabled = false;
-    }
+  if (document.getElementById('insert-text').value == '') {
+    document.getElementById('buttonId').disabled = true;
+  } else {
+    document.getElementById('buttonId').disabled = false;
+  }
 }
 
 function disableButton() {
-    document.getElementById('buttonId').disabled = true;
+  document.getElementById('buttonId').disabled = true;
 }
 
 function charCountClean() {
-    document.getElementById('remainingCharacters').innerHTML = 140;
+  document.getElementById('remainingCharacters').innerHTML = '';
 }
 
 function changeColorCount() {
-    if (0 < value <= 10) {
-        document.getElementById('remainingCharacters').classList.remove('yellowCounter')
-        document.getElementById('remainingCharacters').classList.add('redCounter');
-    } 
-    else if (10 < value <= 20) {
-        document.getElementById('remainingCharacters').classList.add('yellowCounter');
-    }
+  if (0 < value <= 10) {
+    document.getElementById('remainingCharacters').classList.remove('yellowCounter')
+    document.getElementById('remainingCharacters').classList.add('redCounter');
+  }
+  else if (10 < value <= 20) {
+    document.getElementById('remainingCharacters').classList.add('yellowCounter');
+  }
 }
 
 function charCount() {
-    let max = 140;
-    let current = document.getElementById('insert-text').value.length;
-    let value = max - current;
-    if (value >= 0) {
-        document.getElementById('remainingCharacters').innerHTML = value;
-    } else {
-        document.getElementById('buttonId').disabled = 'disabled';
-        document.getElementById('remainingCharacters').innerHTML = value;
-    }
+  let max = 140;
+  let current = document.getElementById('insert-text').value.length;
+  let value = max - current;
+  if (value >= 0) {
+    document.getElementById('remainingCharacters').innerHTML = value;
+  } else {
+    document.getElementById('buttonId').disabled = 'disabled';
+    document.getElementById('remainingCharacters').innerHTML = value;
+  }
 }
 
